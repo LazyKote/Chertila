ae605e16a91b266e5b88bc2f6d916f7db30cc85b
LazyKote <124891704+LazyKote@users.noreply.github.com>
2025-03-05 21:11:59 +0300

    whaaat

--- main.cpp
+++ main.cpp
@@ -1,33 +1,36 @@
 #include <Arduino.h>
 #include <Servo.h>
-float path_length[7][7] = {
-{0, 26.5, 45.5, 49.5, 68.0, 0, 54.0}, 
-{26.5, 0, 26.5, 24.5, 56.5, 0, 46.0}, 
-{49.5, 26.5, 0, 35.5, 34.5, 64.0, 69.1}, 
-{49.5, 24.5, 35.5, 0, 51.0, 33.5, 38.5}, 
-{68.0, 56.5, 34.5, 51.0, 0, 61.0, 89.0},
-{0, 0, 64.0, 33.5, 61.0, 0, 51.0},
-{54.0, 46.0, 69.1, 38.5, 89.0, 51.0, 0}};
+//матрица "расстояний"
+/*float path_length[7][7] = {
+  {0.0, 26.5, 45.5, 49.5, 68.0, 0.0, 54.0}, 
+  {26.5, 0.0, 26.5, 24.5, 56.5, 0.0, 46.0}, 
+  {49.5, 26.5, 0.0, 35.5, 34.5, 64.0, 69.1}, 
+  {49.5, 24.5, 35.5, 0.0, 51.0, 33.5, 38.5}, 
+  {68.0, 56.5, 34.5, 51.0, 0.0, 61.0, 89.0},
+  {0.0, 0.0, 64.0, 33.5, 61.0, 0.0, 51.0},
+  {54.0, 46.0, 69.1, 38.5, 89.0, 51.0, 0.0}};*/
 int dot_chain[] = {1,2,3,4,5}; //порядок точек
-float path_angle[7][7] = {
-  {90, 90, 90, 90, 90, 90, 90}, 
-  {90, 90, 90, 90, 90, 90, 90}, 
-  {90, 90, 90, 90, 90, 90, 90}, 
-  {90, 90, 90, 90, 90, 90, 90}, 
-  {90, 90, 90, 90, 90, 90, 90}, 
-  {90, 90, 90, 90, 90, 90, 90}, 
-  {90, 90, 90, 90, 90, 90, 90}}; //матрица с углами поворота
+/*float path_angle[7][7] = {
+  {0, 90.0, 100.0, 48.0, 90.0, 50.0, 30.0}, 
+  {180.0, 0.0, 90.0, 90.0, 70.0, 30.0, 90.0}, 
+  {100.0, 90.0, 0.0, 10.0, 20.0, 90.0, 90.0}, 
+  {90.0, 120.0, 90.0, 0.0, 100.0, 90.0, 90.0}, 
+  {60.0, 70.0, 80.0, 100.0, 0.0, 200.0, 90.0}, 
+  {90.0, 70.0, 160.0, 90.0, 90.0, 0.0, 90.0}, 
+  {90.0, 90.0, 90.0, 90.0, 90.0, 90.0, 0.0}};*/ //матрица с углами поворота
+  float path_length[] = {0.56, 0.30, 0.20, 0.50, 0.40};
+  float path_angle[] = {0, 90.0, 100.0, 48.0, 90.0};
 Servo servo1;
 int start_button = 0;
-int motor1Pin1 = 14; 
-int motor1Pin2 = 27;
-int speed1Pin = 12;
-int motor2Pin1 = 26; 
-int motor2Pin2 = 25; 
-int speed2Pin = 16;
+int motor1Pin1 = 9; 
+int motor1Pin2 = 8;
+int speed1Pin = 7;
+int motor2Pin1 = 11; 
+int motor2Pin2 = 10; 
+int speed2Pin = 5;
 
-#define CLK_pin 26      // Сигнальный пин энкодера
-#define DT_pin 25       // Сигнальный пин энкодера
+#define CLK_pin 2      // Сигнальный пин энкодера
+#define DT_pin 3       // Сигнальный пин энкодера
 #define discrets 1600.0 // Число импульсов на полный оборот
 #define d 0.095         // Диаметр колеса
 #define PI 3.1415926535 // PI
@@ -58,6 +61,15 @@ void setup_encoder(int clk, int dt) {
   attachInterrupt(dt, updateEncoder, CHANGE);
 }
 
+ //остановка моторов
+ void stop()
+ {
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, LOW);
+ }
+ 
 /*
 Получение линейной скорости с энкодера
 
@@ -79,86 +91,74 @@ float get_speed(int delay_ms) {
 // Функция для поворота на заданный угол
 void turn_angle(float target_angle) {
   float current_angle = 0;
-  long tmr1 = millis();
-  float initial_counter = counter;
-
-  // Коэффициент для перевода импульсов энкодера в градусы
-  float angle_per_pulse = 360.0 / discrets;
+  unsigned long tmr1 = millis();
+  long initial_counter = counter; // Начальное значение счетчика энкодера
+  float angle_per_pulse = 360.0 / discrets; // Градусов на один импульс энкодера
 
   while (abs(current_angle) < abs(target_angle)) {
-    if (millis() - tmr1 > 10) {
-      tmr1 = millis();
-      current_angle = (counter - initial_counter) * angle_per_pulse;
+      if (millis() - tmr1 > 10) { // Обновляем каждые 10 мс
+          tmr1 = millis();
+          current_angle = (counter - initial_counter) * angle_per_pulse;
 
-      if (target_angle > 0) {
-        // Поворот вправо
-        digitalWrite(motor1Pin1, HIGH);
-        digitalWrite(motor1Pin2, LOW);
-        digitalWrite(motor2Pin1, LOW);
-        digitalWrite(motor2Pin2, HIGH);
-      } else {
-        // Поворот влево
-        digitalWrite(motor1Pin1, LOW);
-        digitalWrite(motor1Pin2, HIGH);
-        digitalWrite(motor2Pin1, HIGH);
-        digitalWrite(motor2Pin2, LOW);
-      }
-      analogWrite(speed1Pin, 150);
-      analogWrite(speed2Pin, 150);
+          if (target_angle > 90) {
+              // Поворот вправо
+              digitalWrite(motor1Pin1, HIGH);
+              digitalWrite(motor1Pin2, LOW);
+              digitalWrite(motor2Pin1, LOW);
+              digitalWrite(motor2Pin2, HIGH);
+          } 
+          if (target_angle < 90) {
+              // Поворот влево
+              digitalWrite(motor1Pin1, LOW);
+              digitalWrite(motor1Pin2, HIGH);
+              digitalWrite(motor2Pin1, HIGH);
+              digitalWrite(motor2Pin2, LOW);
+          }
+          analogWrite(speed1Pin, 130); // Скорость моторов
+          analogWrite(speed2Pin, 250);
     }
   }
-  stop(); // Остановка моторов после поворота
+  stop();
 }
 
 //движение моторов вперёд
-void move(float cur_dist)
+void move()
  {
+      Serial.print("Полный вперёд");
       digitalWrite(motor1Pin1, HIGH);
       digitalWrite(motor1Pin2, LOW);
       digitalWrite(motor2Pin1, HIGH);
       digitalWrite(motor2Pin2, LOW);
-      analogWrite(speed1Pin,abs(cur_dist));
-      analogWrite(speed2Pin,abs(cur_dist));
+      analogWrite(speed1Pin, abs(130));
+      analogWrite(speed2Pin, abs(250));
       delay(2);
 }
 
- //остановка моторов
- void stop()
- {
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, LOW);
- }
- 
 // Функция для перемещения вперед на определенное расстояние
 void go_distance(float distance, float angle) {
   float cur_dist = 0;
   long tmr1 = 0;
+  turn_angle(angle);
   while (cur_dist < distance) {
     if (millis() - tmr1 > 10) {
       tmr1 = millis();
       float linear = get_speed(10);
       cur_dist += linear / 100.0;
     }
-    if (angle != 90) {
-      turn_angle(angle);
-    }
-    move(cur_dist);
-    move(cur_dist);
+
+    move();
   }
   cur_dist = 0;
   stop();
 }
  
 //Берём точку начала и конечную точку
-int dot_by_dot(int dot_chain[])
+void dot_by_dot(int dot_chain[])
 {
-    for (int i=0; i<5; i++)
+    for (int i = 0; i < 5; i++)
     {
         int dot1 = dot_chain[i];
-        int dot2 = dot_chain[i+1];
-        go_distance(path_length[dot1][dot2], path_angle[dot1][dot2]);
+        go_distance(path_length[dot1], path_angle[dot1]);
     }
 }
 
@@ -171,12 +171,20 @@ void setup()
   pinMode(motor2Pin2, OUTPUT);
   pinMode(speed2Pin, OUTPUT);
   pinMode(start_button, OUTPUT);
-  servo1.attach(0); //привязываем свервопривод к пину
-  servo1.write(90); //опускаем маркер
+  servo1.attach(13); //привязываем свервопривод к пину
+  servo1.write(150); //опускаем маркер
   setup_encoder(CLK_pin, DT_pin);
+  Serial.begin(9600);
 }
 
 void loop()
 {
- dot_by_dot(dot_chain);
+  /*digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+  digitalWrite(motor2Pin1, HIGH);
+  digitalWrite(motor2Pin2, LOW);
+  analogWrite(speed1Pin,150);
+  analogWrite(speed2Pin,150);
+  delay(2);*/
+  dot_by_dot(dot_chain);
 }
